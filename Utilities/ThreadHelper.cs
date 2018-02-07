8687405c4282ced9d6755651c5733606bd628dd7
Eric <echen@nero.com>
2018-02-07 10:51:15 +0800

    Add ThreadHelper

---
+++ Utilities/ThreadHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.Extension;
+
+namespace Utilities
+{
+    public class ThreadHelper
+    {
+        public static void EndInvokeAction(IAsyncResult result)
+        {
+            Action action = result.AsyncState as Action;
+            if (action.IsNull())
+            {
+                return;
+            }
+            
+            action.EndInvoke(result);
+        }
+
+        public static T EndInvokeFunc<T>(IAsyncResult result)
+        {
+            Func<T> func = result.AsyncState as Func<T>;
+            if (func.IsNull())
+            {
+                return default(T);
+            }
+
+            return func.EndInvoke(result);
+        }
+    }
+}
